e49debc87b7dcc4afbbf6562e3a75c9d0e12e666
Tao Cumplido <taocumplido@gmail.com>
2020-11-16 20:00:22 +0100

    update dependencies / eslint config

--- src/util/types.ts
+++ src/util/types.ts
@@ -4,14 +4,10 @@ export type PartialMap<T extends unknown[]> = {
 	[P in keyof T]?: Partial<T[P]>;
 };
 
-// https://github.com/typescript-eslint/typescript-eslint/issues/1868
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 export interface RuleContext<Configuration extends unknown[]> extends Rule.RuleContext {
 	options: PartialMap<Configuration>;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 export interface RuleModule<Configuration extends unknown[]> extends Rule.RuleModule {
 	create: (context: RuleContext<Configuration>) => Rule.RuleListener;
 }
