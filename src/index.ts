bcba4466db7dcffba4d71f7d1e5e981958723025
Tao Cumplido <taocumplido@gmail.com>
2021-10-27 13:50:58 +0200

    update self dependency

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import { rule as sortImports } from './rules/sort-imports';
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace Reflect {
-		// eslint-disable-next-line @typescript-eslint/ban-types
 		function has<T extends object, P extends PropertyKey>(target: T, propertyKey: P): target is T extends unknown ? (P extends keyof T ? T : never) : never;
 	}
 }
